a4c1a5fe0f8c4c67efecfd860c01b8f812c83df4
shannel890 <shannelkirui739@gmail.com>
2025-05-01 22:35:54 +0300

    modified routes.py,addidas.html and rolex.html

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,8 @@ from flask import render_template
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    return render_template('addidas.html')
+
+@app.route('/rolex')
+def addidas():
+    return render_template('rolex.html')
