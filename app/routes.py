f9e327482feb4c33a0d18491384add47b7eafc75
shannel890 <shannelkirui739@gmail.com>
2025-05-04 23:16:20 +0300

    modified route.py,base.html,dior.html,index.html and added cart.html,form.py and login.html

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 
 
+
 @app.route('/')
 def home():
     return render_template('index.html',title='KIPPS MALL')
@@ -21,4 +22,12 @@ def adidas():
 @app.route('/samsung')
 def samsung():
     return render_template('samsung.html',title='KIPPS MALL')
+
+@app.route('/cart')
+def cart():
+    return render_template('cart.html',title='KIPPS MALL')
+@app.route('/login')
+def login():
+    form = Loginform()
+    return render_template('login.html',title='KIPPS MALL',login_form=form)
 
