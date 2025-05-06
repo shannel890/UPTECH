a9751250b1ae4cbfacac35ceb58c805a42be26ce
shannel890 <shannelkirui739@gmail.com>
2025-05-06 21:08:20 +0300

    modified __init_.py,routes.py,cart.html,forms.py,login.html,__pycache__/__init__.cpython-310.pyc and __pycache__/routes.cpython-310.pyc

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_login import LoginManager
 
 app = Flask (__name__)
+app.config['SECRET_KEY']='ae55cc2714070645b832bb082177889aea7b6556e0c00e8e01ec94c41f8552e0'
+login_manager = LoginManager()
+login_manager.login_view = 'login'
 
 from app import routes
